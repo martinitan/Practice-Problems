6c90c8bd0ccee0796bf06ebc791481c9f719b13c
martinitan <martinitan@hotmail.com>
2023-01-24 23:40:54 -0800

    Passed leetcode
    solved poorly simply in other problem.
    Moved to more efficient algo. roughly same but less duplication of recursion.

---
+++ leetcode50/main.c
@@ -0,0 +1,46 @@
+/* main.c */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// compute power function
+
+double myPow(double x, int n)
+{
+    double temp;
+
+    if( n == 0)
+    {
+        return 1;
+    }
+    temp = myPow(x, n/2);
+    printf("value: %f\n", temp);
+    if(n % 2 == 0)
+    {
+        printf("temp*temp: %f\n", temp);
+        return temp*temp;
+    }
+    else
+    {
+        if(n > 0)
+        {
+            printf("temp*temp*x: %f temp: %f\n", temp*temp*x, temp);
+            return temp*temp*x;
+        }
+        else
+        {
+            printf("temp*temp/x: %f temp: %f\n", temp*temp/x,temp);
+            return temp*temp/x;
+        }
+    }
+
+}
+
+void main(void)
+{
+    double test = 2.0;
+    int exp = 4;
+
+
+    printf("answer: %f\n", myPow(test,exp));
+}
